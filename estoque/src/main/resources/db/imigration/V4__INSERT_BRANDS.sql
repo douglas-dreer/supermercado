5c3cb2a5d91d979d5ea9daf86fbed256400aea20
Douglas Dreer <douglasdreer@outlook.com.br>
2025-09-25 18:48:53 -0300

    feat: Flyway Migration Adicionado os script de imigração para o projeto de estoque - Criação de esquema estoque - Criação de tabela brand - Adicionado dados na tabela de brands

---
+++ estoque/src/main/resources/db/imigration/V4__INSERT_BRANDS.sql
@@ -0,0 +1,5 @@
+INSERT INTO estoque.brands(id, name, description, created_at, updated_at, active)
+VALUES ('226656a4-f445-4f3a-9b7a-70cb98316923', 'Coca Cola Company', 'Coca Cola Company', NOW(), NOW(), true),
+       ('8a1fbb64-63f9-4c18-8a23-67eba8e3c5f8', 'Samsung Eletronics', 'Samsung Eletronics', NOW(), NOW(), true),
+       ('de8d9748-dd29-418e-a97f-77466f3cc98c', 'LG', 'LG', NOW(), NOW(), true),
+       ('04912392-2da4-4c70-a357-1e124cebdba3', 'PepsiCO', 'Pepsi Company', NOW(), NOW(), true);
