1296dbcaef3e42ab8b3f60de9586d7b50ce7728c
Douglas Dreer <douglasdreer@outlook.com.br>
2025-09-25 12:19:36 -0300

    Commit inicial PDV

---
+++ pdv/src/main/kotlin/br/com/supermercado/pdv/PontoDeVendaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.supermercado.pdv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontoDeVendaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PontoDeVendaApplication>(*args)
+}
