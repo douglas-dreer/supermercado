705054d1644d8e0e8426b05297cb4e95da7b744c
Douglas Dreer <douglasdreer@outlook.com.br>
2025-09-26 10:43:30 -0300

    fix: Removido arquivos de script de bd. Foram removido os arquivos de banco de dados, pois serão centralizados no Flyway

--- init-db/01_SCHEMA.sql
+++
@@ -1,6 +0,0 @@
-/*
- Cria os schemas do banco de dados
- */
-CREATE SCHEMA IF NOT EXISTS estoque;
-CREATE SCHEMA IF NOT EXISTS vendas;
-CREATE SCHEMA IF NOT EXISTS financeiro;
--- init-db/02_ADICIONAR_MARCAS.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO estoque.marcas (id, name, logo, created_at) VALUES
-('8f61b3fb-13ae-4853-9b10-f5d40650ee37','Coca Cola', 'https://brandlogos.net/wp-content/uploads/2021/10/Coca-Cola-Coke-logo-512x171.png', NOW()),
-('21d5ebf6-742a-4c04-b70b-83cd92682068','Pepsi', 'https://example.com/pepsi.png', NOW()),
-('23d4fb56-5fe0-42ac-9316-8dd7ebfdb318','Nestlé', 'https://example.com/nestle.png', NOW()),
-('3df31118-e097-453c-90cf-ebd65b9eeab1','Omo', 'https://example.com/omo.png', NOW()),
-('6b9c6450-f43b-4334-8656-93900b98ba97','Garoto', 'https://example.com/garoto.png', NOW());
