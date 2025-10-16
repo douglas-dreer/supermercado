530cb497217a2d6222dbe21b86ede7d34b709349
Douglas Dreer <douglasdreer@outlook.com.br>
2025-10-16 11:40:47 -0300

    [SMK-001][CAT-004] FEAT: Adicionado os validates

---
+++ estoque/src/main/kotlin/br/com/supermercado/estoque/domain/validation/CategoryValidator.kt
@@ -0,0 +1,14 @@
+package br.com.supermercado.estoque.domain.validation
+
+import br.com.supermercado.estoque.infrastructure.adapter.output.persistence.adapter.CategoryRepositoryAdapter
+import java.util.*
+
+abstract class CategoryValidator {
+    fun verifyIfExistId(categoryId: UUID, repository: CategoryRepositoryAdapter): Boolean {
+        return repository.existsById(categoryId)
+    }
+
+    fun isNameRegisteredForOtherCategory(brandName: String, repository: CategoryRepositoryAdapter): Boolean {
+        return repository.existsByName(brandName)
+    }
+}
